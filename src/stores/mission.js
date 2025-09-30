e20edc06925357e4f2c14cd2bf8a6f15ccc1c6d6
James-byds <james.boydens@gmail.com>
2025-09-30 10:14:13 +0200

    init-mission-store

---
+++ src/stores/mission.js
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+import ApiService from "@/services/ApiService";
+
+/**
+ * Mission Store using Pinia
+ *
+ * @description Manages current mission state, global mission fetching and other mission operations
+ * @example
+ * // In a Vue component
+ * import { useMissionStore } from '@/stores/mission'
+ *
+ * const missionStore = useMissionStore()
+ */
+
+export const useMissionStore = defineStore("mission", {
+  /*
+  Store state
+  */
+ state: () => ({
+  
+ })
+})
