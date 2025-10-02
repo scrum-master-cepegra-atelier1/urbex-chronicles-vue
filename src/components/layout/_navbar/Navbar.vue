63c4d5585cc181f2f117869f08f6d48b47907c09
DodolKo <do.evrard.pro@gmail.com>
2025-10-02 10:51:55 +0200

    :tada: fix the ERROR 426 for mmy laptop

--- src/components/layout/_navbar/Navbar.vue
+++ src/components/layout/_navbar/Navbar.vue
@@ -10,21 +10,31 @@ import Icon from '@/components/ui/_Icon/Icon.vue'
 <template>
   <nav class="navigation-container">
     <ul class="navigation-list">
-      <li class="navigation-list__item"> <a href="" class="navigation-label" id="">
-        <Icon name="settings_accessibility" size="2xl" dir="icon" />
-        Profil</a></li>
-      <li class="navigation-list__item"> <a href="" class="navigation-label" id="">
-        <Icon name="flag" size="2xl" dir="icon" />
-        Mission</a></li>
-      <li class="navigation-list__item"> <a href="" class="navigation-label" id="">
-        <Icon name="settings" size="2xl" dir="icon" />
-        Settings</a></li>
-      <li class="navigation-list__item"> <a href="" class="navigation-label" id="">
-        <Icon name="map" size="2xl" dir="icon" />
-        Maps</a></li>
-      <li class="navigation-list__item"> <a href="" class="navigation-label" id="">
-        <Icon name="trophy" size="2xl" dir="icon" />
-        Succes</a></li>
+      <li class="navigation-list__item"> 
+        <router-link to="" class="navigation-label" id="">
+          <Icon name="settings_accessibility" size="2xl" dir="icon" />
+          Profil
+        </router-link></li>
+      <li class="navigation-list__item"> 
+        <router-link to="" class="navigation-label" id="">
+          <Icon name="flag" size="2xl" dir="icon" />
+          Mission
+        </router-link></li>
+      <li class="navigation-list__item"> 
+        <router-link to="" class="navigation-label" id="">
+          <Icon name="settings" size="2xl" dir="icon" />
+          Settings
+        </router-link></li>
+      <li class="navigation-list__item"> 
+        <router-link to="" class="navigation-label" id="">
+          <Icon name="map" size="2xl" dir="icon" />
+          Maps
+        </router-link></li>
+      <li class="navigation-list__item"> 
+        <router-link to="" class="navigation-label" id="">
+          <Icon name="trophy" size="2xl" dir="icon" />
+          Succes
+        </router-link></li>
     </ul>
   </nav>
 </template>
