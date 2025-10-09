3b7fa1ee992db736316aecf28367509ddf8b9e89
dev_vens <vensalex1991@gmail.com>
2025-10-09 12:56:35 +0200

    feat: enhance QuestionCard and GameRunningView for dynamic questionnaire handling

--- src/components/layout/_MissionCard/MissionCard.vue
+++ src/components/layout/_MissionCard/MissionCard.vue
@@ -1,31 +1,19 @@
 <template>
-<section class="mission-card" :class="'mission-card--'+props.displayMode">
-  <figure class="mission-card__image">
-    <img src="/data/urbex.jpg" alt="Mission Image" class="mission-card__image__img"/>
-  </figure>
-  <aside class="mission-card__info">
-    <h2 class="mission-card__info__title">{{ props.mission.name}}</h2>
-    <p class="mission-card__info__description">{{ props.mission.description }}</p>
-    <button class="mission-card__info__start-button" v-if="props.displayMode === 'squared'">Start Mission</button>
-  </aside>
-</section>
+  <section class="mission-card" :class="'mission-card--' + props.displayMode">
+    <figure class="mission-card__image">
+      <img src="/data/urbex.jpg" alt="Mission Image" class="mission-card__image__img" />
+    </figure>
+    <aside class="mission-card__info">
+      <h2 class="mission-card__info__title">{{ props.mission.name }}</h2>
+      <p class="mission-card__info__description">{{ props.mission.description }}</p>
+      <button class="mission-card__info__start-button" v-if="props.displayMode === 'squared'">
+        Start Mission
+      </button>
+    </aside>
+  </section>
 </template>
 
 <script setup>
-//import api
-import { useCircuitStore } from '@/stores/circuit';
-import { onBeforeMount } from 'vue';
-
-const circuitStore = useCircuitStore();
-
-onBeforeMount(async () => {
-  // switch to circuits store fetching if needed by this component
-  if (typeof circuitStore.getCircuits === 'function') {
-    await circuitStore.getCircuits();
-    console.log(circuitStore.circuits);
-  }
-})
-
 //define props for display mode
 const props = defineProps({
   displayMode: {
@@ -33,21 +21,24 @@ const props = defineProps({
     default: 'squared',
     validator(value) {
       return ['squared', 'long'].includes(value) //only allow these two values
-    }
+    },
   },
   mission: {
     type: Object,
     default: () => ({
       name: 'Default Mission',
-      description: 'Default mission description'
+      description: 'Default mission description',
     }),
   },
+})
+
+console.log({
+  displayMode: props.displayMode,
+  mission: props.mission,
 })
 </script>
 
 <style>
-
-    /* Tailwind ne prend pas c'est @apply directement dans le style !*/
-    @import './MissionCard.css';
-
+/* Tailwind ne prend pas c'est @apply directement dans le style !*/
+@import './MissionCard.css';
 </style>
