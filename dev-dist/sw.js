81cf2c67b96d1f7d1ee22ca7c9b5b4fb2373ba95
dev_vens <vensalex1991@gmail.com>
2025-10-13 15:36:15 +0200

    :bug: update redirect after successful login to Home in LoginView

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m6m5el5ttp"
+    "revision": "0.jis16e4om2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
