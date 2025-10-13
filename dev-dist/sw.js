bd453f7dbfcb9ee38d6ac7dd33f19d1f43c07084
dev_vens <vensalex1991@gmail.com>
2025-10-13 15:42:15 +0200

    :bug: update revision for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jis16e4om2"
+    "revision": "0.2jbfeqgn4jg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
